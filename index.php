62f345a3e6de84e427800abca28936839a953242
EricJohnAgapen <agapeneric@gmail.com>
2022-09-24 09:34:26 +0800

    initials

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Hands On Activity - ODL</title>
+</head>
+<body>
+<h4>PHP Array Exercise 1</h4>
+
+<?php
+
+?>
+
+</body>
+</html>
